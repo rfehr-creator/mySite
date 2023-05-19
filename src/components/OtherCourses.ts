a47283e4be8a6ea25cd062e5836b14ffaa8b76d5
rfehr-creator <you@rfk4plt@gmail.com>
2023-05-18 19:44:48 -0500

    Style education page

---
+++ src/components/OtherCourses.ts
@@ -0,0 +1,24 @@
+export const otherCourses = [
+  {
+    where: "Udemy",
+    courseName: "Unit Testing for C# Developers",
+  },
+  {
+    where: "Udemy",
+    courseName: "C# Advanced Topics: Prepare for Technical Interviews",
+  },
+  {
+    where: "Udemy",
+    courseName: "Ultimate ASP.NET Core Web API Development Guide",
+  },
+  {
+    where: "Udemy",
+    courseName: "Make an E-Commerce Website with Blazor WebAssembly",
+  },
+  {
+    where: "codewithmosh",
+    courseName: "React 18 for Beginners",
+  },
+];
+
+export default otherCourses;
--- src/pages/EducationPage.tsx
+++ src/pages/EducationPage.tsx
@@ -1,46 +1,92 @@
 import styled from "styled-components";
-import courses from "../components/Courses";
-const Table = styled.table`
-  
+import courses from "../components/UMCourses";
+import otherCourses from "../components/OtherCourses";
+const padding = "5px;";
+
+const Table = styled.table``;
+
+const Card = styled.div`
+  width: fit-content;
+`;
+
+const Header = styled.thead`
+  background-color: black;
+  color: white;
+`;
+
+const Footer = styled.tfoot`
+  background-color: black;
+  color: white;
+`;
+const TH = styled.th`
+  padding: ${padding};
+`;
+const TD = styled.td`
+  padding: ${padding};
 `;
 
 const Row = styled.tr`
-
+  :nth-child(even) {
+    background-color: #d1ebfc;
+  }
 `;
 export const EducationPage = () => {
   return (
-    <div className="card text-white bg-black m-3">
+    <div>
+      <Card className="card m-3">
       <div className="card-body">
-        <h5 className="card-title">University of Manitoba</h5>
-        <Table>
-          <thead>
-            <Row>
-              <th>#</th>
-              <th>Course Number</th>
-              <th>Course Completed</th>
-              <th>Grade</th>
-            </Row>
-          </thead>
-          <tbody>
-            {courses.map((course, index) => (
+          <h2 className="card-title">Other</h2>
+          <Table>
+            <Header>
               <Row>
-                <td>{index + 1}</td>
-                <td>{course.courseId}</td>
-                <td>{course.courseName}</td>
-                <td>{course.grade}</td>
+                <TH>Where</TH>
+                <TH>Course Name</TH>
               </Row>
-            ))}
-          </tbody>
-          <tfoot>
-            <Row>
-              <td>GPA</td>
-              <td></td>
-              <td></td>
-              <td>4.28 of 4.5</td>
-            </Row>
-          </tfoot>
-        </Table>
-      </div>
+            </Header>
+            <tbody>
+              {otherCourses.map((course) => (
+                <Row>
+                  <TD>{course.where}</TD>
+                  <TD>{course.courseName}</TD>
+                </Row>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+      </Card>
+      <Card className="card m-3">
+        <div className="card-body">
+          <h2 className="card-title">University of Manitoba</h2>
+          <Table>
+            <Header>
+              <Row>
+                <TH>#</TH>
+                <TH>Course #</TH>
+                <TH>Course Name</TH>
+                <TH>Grade</TH>
+              </Row>
+            </Header>
+            <tbody>
+              {courses.map((course, index) => (
+                <Row>
+                  <TD>{index + 1}</TD>
+                  <TD>{course.courseId}</TD>
+                  <TD>{course.courseName}</TD>
+                  <TD>{course.grade}</TD>
+                </Row>
+              ))}
+            </tbody>
+            <Footer>
+              <Row>
+                <td>GPA</td>
+                <td></td>
+                <td></td>
+                <td>4.28 of 4.5</td>
+              </Row>
+            </Footer>
+          </Table>
+        </div>
+      </Card>
     </div>
   );
 };
